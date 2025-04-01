85e22a551c34124b9a9ebda1973451164e6662c6
danielpolland <S00249853@atu.ie>
2025-04-01 20:49:48 +0100

    Add project files.

---
+++ ExamPaper2024OOD/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamPaper2024OOD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Customer c1 = new Customer() { Name = "Tom Jones", ContactNumber = "086-123 4567" };
+        //Customer c2 = new Customer() { Name = "Mary Smith", ContactNumber = "086 546 3214" };
+        //Customer c3 = new Customer() { Name = "Jo Doyle", ContactNumber = "087 1221 222" };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
